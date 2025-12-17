ab93055cf16e3888b3f51d619c656d395360235f
aditya <adityash549@gmail.com>
2025-12-18 02:00:23 +0530

    testrrr

--- OrdersApi/Program.cs
+++ OrdersApi/Program.cs
@@ -3,8 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashb
 builder.Services.AddSwaggerGen
 
 // Configure the HTTP request pipeline.
@@ -16,7 +15,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseAuthorization
 
 app.MapControllers();
 
